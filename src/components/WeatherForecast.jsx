74f6b0eaaf1d9e7029172cd596931d3b773b743f
Emma Frith <emma.frith@education.gov.uk>
2024-06-03 15:56:40 +0100

    core content

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,19 @@
+import '../WeatherForecast.css'
+
+function WeatherForecast( { key, day, img, imgAlt, conditions, time}) {
+
+return (
+
+<div className="weather">
+  <h2>{day}</h2>
+  <img src={img} alt={imgAlt} />
+  <p><span>conditions: </span>{conditions}</p>
+  <p><span>time: </span>{time}</p>
+</div>
+
+)
+
+}
+
+
+export default WeatherForecast
